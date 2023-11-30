1a37eeabb006ab8831f16cf32afdfd22d83c02a4
xamat <xavier@amatriain.net>
2023-11-30 15:24:47 -0800

    Finally got OpenAI chat API to work after merge

--- src/server/app.js
+++ src/server/app.js
@@ -14,28 +14,46 @@ app.post('/chat', async (req, res) => {
 
   // Here, you should process the userMessage and generate a response.
   // For debugging, let's just send back a simple message:
-  const responseMessage = "Received your message: " + userMessage;
-  res.json({ message: responseMessage });
+  //const responseMessage = "Received your message: " + userMessage;
+  //res.json({ message: responseMessage });
 
 
-//  try {
-//    const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
-//      prompt: userMessage,
-//      max_tokens: 150,
-//    }, {
-//      headers: {
-//        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
-//      }
-//    });
+  try {
+  const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+    model: "gpt-3.5-turbo", // Or your chosen conversational model
+    messages: [{ role: "user", content: userMessage }] // Wrap user message in 'messages' array
+  }, {
+    headers: {
+      'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+    }
+  });
 
-//   const aiMessage = response.data.choices[0].text.trim();
-//    res.json({ message: aiMessage });
-//  } catch (error) {
-//    res.status(500).json({ message: 'Error communicating with OpenAI' });
-//  }
+  if (response.data.choices && response.data.choices.length > 0) {
+    // Extract the message object
+    const messageObject = response.data.choices[0].message;
+
+    if (messageObject) {
+      // Assuming the message object contains a property like 'content' with the response text
+      const aiMessage = messageObject.content ? messageObject.content.trim() : "";
+      res.json({ message: aiMessage });
+    } else {
+      res.status(500).json({ message: "Received an unexpected response format from OpenAI API." });
+    }
+  } else {
+    res.status(500).json({ message: "No choices found in the response from OpenAI API." });
+  }
+} catch (error) {
+  console.error("OpenAI API error:", error.response ? error.response.data : error.message);
+  let errorMessage = 'Error communicating with OpenAI';
+  if (error.response && error.response.data) {
+    errorMessage += `: ${error.response.data.error}`;
+  }
+  res.status(500).json({ message: errorMessage });
+}
 });
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
