0d3cefe1fc12f1413b3870fafdd5c2009638322c
xamat <xavier@amatriain.net>
2023-12-12 16:48:12 -0800

    I am now getting a response from assistant in the backend

--- src/server/app.js
+++ src/server/app.js
@@ -101,16 +101,6 @@ async function createRun(req,res) {
   }
 };
 
-async function add_message_to_thread(thread_id, user_question) {
-    // Create a message inside the thread
-    console.log('Adding message to thread:', user_question);
-    message = await openai.beta.threads.messages.create(
-        thread_id,{
-        role: "user",
-        content: user_question,
-        });
-    return message;
-};
 
 async function get_answer(thread){
     print("Thinking...");
@@ -192,12 +182,18 @@ app.post('/chatWithAssistant', async (req, res) => {
   try {
   
   //First we add the user message to the thread
-  console.log('Adding message to thread');
-   await openai.beta.threads.messages.create(threadId, {
+  console.log('Adding message to thread:', userMessage);
+  const threadMessages = await openai.beta.threads.messages.create(threadId, {
     role: 'user',
     content: userMessage,
     });
-  //await add_message_to_thread(threadId, userMessage);
+  
+  //making sure I can retrieve message for debugging
+  /* const message = await openai.beta.threads.messages.retrieve(
+    threadId,
+    threadMessages.id
+  );
+  console.log(message); */
 
   let timeElapsed = 0;
   const timeout = 60000; // Timeout in milliseconds
@@ -209,26 +205,30 @@ app.post('/chatWithAssistant', async (req, res) => {
     { assistant_id: assistantId }
   );
   console.log('Starting poll to wait for run to finish');
-  
   while (timeElapsed < timeout) {
+    console.log('timeElapsed:', timeElapsed);
     console.log('Getting run info with threadId, runId',threadId,run.id);
-    const runInfo = await openai.beta.threads.runs.retrieve(thread_id=threadId, run_id=run.id);
+    const runInfo = await openai.beta.threads.runs.retrieve(threadId, run.id);
     console.log('Got run information from openAI with run status:', runInfo.status);
     if (runInfo.status === 'completed') {
-      const messagesFromThread = await openai.beta.threads.messages.list(threadId);
+      messagesFromThread = await openai.beta.threads.messages.list(threadId);
       // Resolve or handle the completed run
       console.log('Run completed');
-      console.log({ runResult: run, messages: messagesFromThread });
-      return;
+      //console.log({ runResult: run, messages: messagesFromThread });
+      break;
     }
     await new Promise(resolve => setTimeout(resolve, interval));
     timeElapsed += interval;
   }
 
-  if (response.data.choices && response.data.choices.length > 0) {
+  //console.log('Attempting to extract messages from response', messagesFromThread);
+  console.log('First message is:', messagesFromThread.data[0].content[0].text.value);
+  
+  if (messagesFromThread.data.length > 0 && messagesFromThread.data[0].content.length > 0) {
     // Extract the message object. For now I will only process the first message
-    const messageObject = messagesFromThread[0];
-
+    console.log('Response has some messages');
+    const messageObject = messagesFromThread.data[0];
+    console.log('Message object', messageObject);
     if (messageObject) {
       // Assuming the message object contains a property like 'content' with the response text
       console.log('Extracting first message from message list');
@@ -240,6 +240,7 @@ app.post('/chatWithAssistant', async (req, res) => {
     }
   } 
   else {
+    console.log('Response has no messages');
     res.status(500).json({ message: "No choices found in the response from OpenAI API." });
   }
 }
