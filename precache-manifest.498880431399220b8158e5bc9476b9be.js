59fd74a03cbad8b88ae35708c5e957a2f4ee9b55
Xavier Amatriain <xavier@amatriain.net>
2024-07-28 22:27:38 -0700

    Updates

---
+++ precache-manifest.498880431399220b8158e5bc9476b9be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e8368f0484a0e03af5811949595b715",
+    "url": "/Xavibot/index.html"
+  },
+  {
+    "revision": "f03b4b139314cc5e0b5a",
+    "url": "/Xavibot/static/css/2.3f602dff.chunk.css"
+  },
+  {
+    "revision": "ed2270a5e2b39c5bc31d",
+    "url": "/Xavibot/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "f03b4b139314cc5e0b5a",
+    "url": "/Xavibot/static/js/2.6e8e4647.chunk.js"
+  },
+  {
+    "revision": "e26de4f0e0d98f1626b38316e0457409",
+    "url": "/Xavibot/static/js/2.6e8e4647.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed2270a5e2b39c5bc31d",
+    "url": "/Xavibot/static/js/main.7b4c2ba1.chunk.js"
+  },
+  {
+    "revision": "73a8e80d31b15fea4429",
+    "url": "/Xavibot/static/js/runtime-main.b1cf2377.js"
+  }
+]);
